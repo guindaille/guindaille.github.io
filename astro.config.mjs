d84252e8e65321174931da4097ef3e32faaa8c8c
Bruno Boi <boi@odoo.com>
2022-11-10 18:06:02 +0100

    feat(astro): add sitemap.xml generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://guindaille.github.io",
+  integrations: [sitemap()]
 });
