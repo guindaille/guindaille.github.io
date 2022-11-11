54d6052dacdd4666a76dcbac4366d01ba7f67156
Bruno Boi <boi@odoo.com>
2022-11-11 23:04:43 +0100

    feat: add remark-toc
    Usage: add a heading containing "sommaire" in any markdown file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
+import remarkToc from "remark-toc";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://guindaille.github.io",
+  markdown: {
+    extendDefaultPlugins: true,
+    remarkPlugins: [
+      [remarkToc, { tight: true, heading: "Sommaire" }],
+    ],
+  },
   integrations: [sitemap()]
 });
